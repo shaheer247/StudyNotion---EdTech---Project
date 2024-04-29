2e6e55d766c94fe953eb6f49faf900b0bb4d85f2
Shaik Shaheer <137371406+shaheer247@users.noreply.github.com>
2024-04-29 22:25:12 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://study-notion-ed-tech-project-shaheer247-vercel.vercel.app/api/v1";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
