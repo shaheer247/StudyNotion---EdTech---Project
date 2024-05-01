24ebb9c1a38f8118f444b1face50faece49000dc
Shaik Shaheer <137371406+shaheer247@users.noreply.github.com>
2024-05-01 11:51:28 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ by Bhushan Patil  © 2024 Studynotion</div>
+          <div className="text-center">Made with ❤️ by Shaheer Shaik © 2024 Studynotion</div>
         </div>
       </div>
     </div>
